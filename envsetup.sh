8d1c6d12607afa6372ced9a2821e8687cb97ab8b
Greg Prisament <greg@canopy.link>
2014-12-15 02:26:04 +0000

    Fix envsetup to use export

--- envsetup.sh
+++ envsetup.sh
@@ -1,3 +1,3 @@
-# Run from root of repo with: "soruce envsetup.sh"
-CANOPY_EMBEDDED_ROOT=$PWD
-LD_LIBRARY_PATH=$CANOPY_EMBEDDED_ROOT/build/_out/lib
+# Run from root of repo with: "source build/envsetup.sh"
+export CANOPY_EMBEDDED_ROOT=$PWD
+export LD_LIBRARY_PATH=$CANOPY_EMBEDDED_ROOT/build/_out/lib
